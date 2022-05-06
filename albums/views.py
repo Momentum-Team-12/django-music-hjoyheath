010508b1ba8968bc708ce34e60c7f61a7c4ffd5e
hjoyheath <hillaryjoyheath@gmail.com>
2022-05-06 11:36:46 -0400

    button add on detail page and links to edit, delete pages

--- albums/views.py
+++ albums/views.py
@@ -26,12 +26,13 @@ def detail_album(request, pk):
     return render(request, "albums/detail_album.html", {"album": album})
 
 
-def delete_album(request, pk):
+def delete_albums(request, pk):
     album = get_object_or_404(Album, pk=pk)
     if request.method == 'POST':
         album.delete()
         return redirect(to='list_albums')
     return render(request, "albums/delete_albums.html", {"album": album})
+
 
 def edit_album(request, pk):
     album = get_object_or_404(Album, pk=pk)
@@ -42,7 +43,7 @@ def edit_album(request, pk):
         if form.is_valid():
             form.save()
             return redirect(to='list_albums')
-    return render(request, "albums/edit_album.html", {"form": form}, {"album": album})
+    return render(request, "albums/edit_album.html", {"form": form, "album": album})
 
 
         
--- django_music/urls.py
+++ django_music/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', albums_views.list_albums, name='list_albums'),
     path('albums/add', albums_views.add_album, name='add_album'),
-    path('albums/<int:pk>', albums_views.detail_album, name='album_detail'),
-    path('albums/<int:pk>/delete', albums_views.delete_album, name='delete_album'),
+    path('albums/<int:pk>', albums_views.detail_album, name='detail_album'),
+    path('albums/<int:pk>/delete', albums_views.delete_albums, name='delete_albums'),
     path('albums/<int:pk>/edit', albums_views.edit_album, name='edit_album'),
 ]
